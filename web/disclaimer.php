a5df21dada688460cc98d146c7981cd026139989
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2023-08-25 05:50:14 +0000

    wip

--- web/disclaimer.php
+++ web/disclaimer.php
@@ -26,12 +26,19 @@ $header = getHeader("Disclaimer");
 <div class="container info-page-container scec-main-container">
 
     <h1>Disclaimer</h1>
-
     <div>
-		<p>The Community Stress Model(CSM) Viewer Tool is provided “as is” and without warranties of any kind. While <a href="https://www.scec.org">SCEC</a> and the <a href="https://www.scec.org/research/csm">the CSM development team</a> have made every effort to provide data from reliable sources or methodologies, SCEC and the CSM development team do not make any representations or warranties as to the accuracy, completeness, reliability, currency, or quality of any data provided herein. SCEC and the CSM development team do not intend the results provided by this tool to replace the sound judgment of a competent professional, who has knowledge and experience in the appropriate field(s) of practice. By using this tool, you accept to release SCEC and the CSM development team of any and all liability.</p>
-
-		<p>More information about the CSM can be found at: <a href="https://www.scec.org/research/csm">https://www.scec.org/research/csm</a>.</p>
-  </div>
+       <p>The Community Stress Model (CSM) Viewer Tool is provided “as is” and without warranties 
+          of any kind. While <a href="https://www.scec.org">SCEC</a> and
+	  <a href="https://www.scec.org/research/csm">the CSM development team</a> have made every
+	  effort to provide data from reliable sources or methodologies, SCEC and the CSM development
+	  team do not make any representations or warranties as to the accuracy, completeness, 
+	  reliability, currency, or quality of any data provided herein. SCEC and the CSM development
+	  team do not intend the results provided by this tool to replace the sound judgment of a
+	  competent professional, who has knowledge and experience in the appropriate field(s) of 
+	  practice. By using this tool, you accept to release SCEC and the CSM development team of 
+          any and all liability.</p>
+       <p>More information about the CSM can be found at: <a href="https://www.scec.org/research/csm">https://www.scec.org/research/csm</a>.</p>
+    </div>
 </div>
 </body>
 </html>
