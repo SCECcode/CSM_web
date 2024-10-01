4e5af7f51aec6857dbff678715e120fcf2943f3a
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2024-10-01 06:04:38 +0000

    added the new stress models

--- schema/scripts/sql_template/setup_model_schema.sql
+++ schema/scripts/sql_template/setup_model_schema.sql
@@ -5,12 +5,12 @@ CREATE TABLE %%csmtb%_tb (
    LAT     float DEFAULT 0.0,
    DEP     float DEFAULT 0.0,  
 
-   See        float NOT NULL,
-   Sen        float NOT NULL,
-   Seu        float NOT NULL,
-   Snn        float NOT NULL,
-   Snu        float NOT NULL,
-   Suu        float NOT NULL,
+   See        float,
+   Sen        float,
+   Seu        float,
+   Snn        float,
+   Snu        float,
+   Suu        float,
 
    SHmax      float,
    SHmax_unc  float,
@@ -26,25 +26,25 @@ CREATE TABLE %%csmtb%_tb (
    S2         float,
    S3         float,
 
-   V1x        float NOT NULL,
-   V1y        float NOT NULL,
-   V1z        float NOT NULL,
+   V1x        float,
+   V1y        float,
+   V1z        float,
 
-   V2x        float NOT NULL,
-   V2y        float NOT NULL,
-   V2z        float NOT NULL,
+   V2x        float,
+   V2y        float,
+   V2z        float,
 
-   V3x        float NOT NULL,
-   V3y        float NOT NULL,
-   V3z        float NOT NULL,
+   V3x        float,
+   V3y        float,
+   V3z        float,
 
-   V1pl       float NOT NULL,
-   V2pl       float NOT NULL,
-   V3pl       float NOT NULL,
+   V1pl       float,
+   V2pl       float,
+   V3pl       float,
 
-   V1azi      float NOT NULL,
-   V2azi      float NOT NULL,
-   V3azi      float NOT NULL
+   V1azi      float,
+   V2azi      float,
+   V3azi      float
 );
 SELECT AddGeometryColumn('','%%csmtb%_tb','geom','0','POINT',2);
 
